669c40f41cb14856754897b1e0f79a6090279a97
MaJunChaoGit <354488954@qq.com>
2018-11-16 00:14:41 +0800

    补充一下

--- Generator-learning/js/revise.js
+++ Generator-learning/js/revise.js
@@ -160,5 +160,36 @@ function run(gen) {
     if (result.done) return;
     result.value(next);
   }
+
   next();
+}
+
+// 写一个基于Promise的自动执行的Generator(co源码)
+function co(gen) {
+  var ctx = this;
+  return new Promise(function(resolve, reject) {
+    if (typeof gen === 'function') gen = gen.call(ctx);
+    if (!gen || typeof gen.next !== 'function') return resolve(gen.value);
+
+    onFulfilled();
+    function onFulfilled(res) {
+      var ret;
+      try {
+        ret = gen.next(res);
+      } catch(e) {
+        // statements
+        return reject(e);
+      }
+      next(ret);
+    }
+
+    function next(ret) {
+      if (ret.done) return resolve(ret.value);
+      var value = toPromise(ctx, ret.value);
+      if (value && isPromise(value)) return value.then(onFulfilled, onRejected);
+      return onRejected(
+        new Error()
+      );
+    }
+  })
 }
